0bc50cd3270f71d8dbe879c8bdabe4accb309dc6
Gaurav Borkar <gaurav.m.borkar@gmail.com>
2022-01-31 18:19:08 +0530

    Added tax

--- contracts/libraries/SafeMath.sol
+++ contracts/libraries/SafeMath.sol
@@ -29,4 +29,10 @@ library SafeMath {
     assert(c >= a);
     return c;
   }
+
+  function ceil(uint256 a, uint256 m) internal pure returns (uint256) {
+    uint256 c = add(a,m);
+    uint256 d = sub(c,1);
+    return mul(div(d,m),m);
+  }
 }
